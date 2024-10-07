902972a222005557fb8add3233cf840fc4c34879
unknown <kothurutejashwini@gmail.com>
2024-10-07 17:49:54 +0530

    birthdaycard

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import data from './db';
+import "./Style.css"
+
+let App = () => {
+    let [state, setState] = useState(data);
+
+    return (
+        <div id='container'>
+            <h1 id='head'>{state.length} Birthdays Today</h1>
+            <div id='pic'>
+                {state.map(x => {
+                    return (
+                        <div key={x.id} id='box'>
+                            <img src={x.image}  className="birthday-image" alt="pic" />
+                            <ruby id='name'>{x.name}<br/><rt>{x.age} years</rt></ruby>
+                        </div>
+                    );
+                })}
+            </div>
+            <div id='but'>
+                <button onClick={Clear} className="clear-button">Clear All</button>
+            </div>
+        </div>
+    );
+
+    function Clear() {
+        setState([]);
+    }
+}
+
+export default App;
